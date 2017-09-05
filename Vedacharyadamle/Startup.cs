5604c446f98fb1a65a88a8355b06ab2a533c0e40
Wily Fox Technologies <rdongre@wilyfox.com>
2017-09-04 17:18:40 -0700

    Add project files.

---
+++ Vedacharyadamle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vedacharyadamle.Startup))]
+namespace Vedacharyadamle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
